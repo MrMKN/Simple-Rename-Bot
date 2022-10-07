c515c5447accc4d86cca98a99420fc13e01cbb16
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-07 20:32:46 +0530

    Create rename.py

---
+++ main/rename.py
@@ -0,0 +1,37 @@
+import time 
+from pyrogram import Client, filters, enums
+from config import temp, CAPTION, ADMIN
+from main.utils import progress_message, humanbytes
+
+@Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))             
+async def rename_file(bot, msg):
+    reply = msg.reply_to_message
+    if len(msg.command) < 2 or not reply:
+       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    media = reply.document or reply.audio or reply.video
+    if not media:
+       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    og_media = getattr(reply, reply.media.value)
+    new_name = msg.text.split(" ", 1)[1]
+    sts = await msg.reply_text("Trying to Downloading.....")
+    c_time = time.time()
+    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
+    filesize = humanbytes(og_media.file_size)                
+    if CAPTION:
+        cap = CAPTION.format(file_name=new_name, file_size=filesize)
+    else:
+        cap = f"{new_name}\n\n💽 size : {filesize}"
+    raw_thumbnail = temp.THUMBNAIL 
+    if raw_thumbnail:
+        og_thumbnail = await bot.download_media(raw_thumbnail)
+    else:
+        og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
+    await sts.edit("Trying to Uploading")
+    c_time = time.time()
+    await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))                                    
+    try:
+        os.remove(downloaded)
+        os.remove(og_thumbnail)
+    except:
+        pass
+    await sts.delete()
