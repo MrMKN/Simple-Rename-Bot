0a8a77a4d945b3b33815fd76d63b942e67a396ca
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-07 19:37:02 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+from pyrogram import Client
+from config import *
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="simple-renamer",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "main"},
+            sleep_threshold=5,
+        )
+
+bot = Bot()
+bot.run()
