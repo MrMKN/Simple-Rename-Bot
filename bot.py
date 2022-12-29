379b0ebd1d0a341f70489d960b9e36886f5069db
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-12-30 01:50:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 from config import *
+from route import web_server
+from aiohttp import web
 
 class Bot(Client):
     def __init__(self):
@@ -10,16 +12,21 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             workers=100,
             plugins={"root": "main"},
-            sleep_threshold=5,
+            sleep_threshold=10,
         )
     async def start(self):
-       await super().start()
-       me = await self.get_me()       
-       print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+        await super().start()
+        me = await self.get_me()   
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        port = "8080"
+        await web.TCPSite(app, bind_address, port).start()   
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
        
     async def stop(self, *args):
-      await super().stop()      
-      print("Bot Restarting........")
+       await super().stop()      
+       print("Bot Restarting........")
 
 
 bot = Bot()
