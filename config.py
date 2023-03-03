074889ea5a9c97b60d7421cd33bcf4fd52424e7b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-03-03 22:38:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
+DOWNLOAD_LOCATION = "./DOWNLOADS"
