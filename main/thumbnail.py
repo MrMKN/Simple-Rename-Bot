e406056cb40504adb002ffd5f5098e3ff5d4ad7b
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-03-03 23:00:39 +0530

    Update thumbnail.py

--- main/thumbnail.py
+++ main/thumbnail.py
@@ -1,23 +1,30 @@
 from pyrogram import Client, filters 
-from config import ADMIN, temp
+from config import ADMIN, DOWNLOAD_LOCATION
+import os
 
-@Client.on_message(filters.private & filters.command("set") & filters.user(ADMIN))                            
-async def set_tumb(bot, msg):
-    replied = msg.reply_to_message
-    if not replied:
-        await msg.reply("use this command with Reply to a photo")
-        return
-    if not msg.reply_to_message.photo:
-       await msg.reply("Oops !! this is Not a photo")
-       return
-    Tumb = msg.reply_to_message.photo.file_id
-    temp.THUMBNAIL = Tumb
-    return await msg.reply(f"Temporary Thumbnail saved✅️ \nDo You want permanent thumbnail. \n\n`{Tumb}` \n\n👆👆 please add this id to your server enviro with key=`THUMBNAIL`")            
+@Client.on_message(filters.private & filters.photo & filters.user(ADMIN))                            
+async def set_tumb(bot, msg):     
+    dl_location = f"{DOWNLOAD_LOCATION}/thumbnail.jpg" 
+    if dl_location:
+        os.remove(dl_location)
+    await bot.download_media(message=msg.photo.file_id, file_name=dl_location)                  
+    return await msg.reply(f"Your permanent thumbnail is saved in dictionary ✅️ \nif you change yur server or recreate the server app to again reset your thumbnail⚠️")            
 
 
 @Client.on_message(filters.private & filters.command("view") & filters.user(ADMIN))                            
+async def view_tumb(bot, msg):
+    thumb = f"{DOWNLOAD_LOCATION}/thumbnail.jpg" 
+    if thumb:
+        await msg.reply_photo(photo=thumb, caption="this is your current thumbnail")
+    else:
+        await msg.reply_text(text="you don't have any thumbnail")
+
+
+@Client.on_message(filters.private & filters.command(["del", "del_thumb"]) & filters.user(ADMIN))                            
 async def del_tumb(bot, msg):
-    if temp.THUMBNAIL:
-        await msg.reply_photo(photo=temp.THUMBNAIL, caption="this is your current thumbnail")
+    thumb = f"{DOWNLOAD_LOCATION}/thumbnail.jpg" 
+    if thumb:
+        os.remove(thumb)
+        await msg.reply_text("your thumbnail was removed🚫")
     else:
         await msg.reply_text(text="you don't have any thumbnail")
