534f5cb3e6447abbfff4428dd252a53afe52db97
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-03-03 23:46:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
+
+# for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+try: Thumbnail = f"{DOWNLOAD_LOCATION}/thumbnail.jpg"
+except: Thumbnail = None
